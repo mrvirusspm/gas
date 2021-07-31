1fe76fdbd2f55c9836c041956e628bbb3f698152
PROGRAMMER <79271699+mrvirusspm@users.noreply.github.com>
2021-08-01 05:52:36 +0700

    Add files via upload

---
+++ login.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+figlet welcome | lolcat
+echo "=================================================" | lolcat
+echo "       My Team Mafia Teknologi Indonesia" | lolcat
+echo "=================================================" | lolcat
